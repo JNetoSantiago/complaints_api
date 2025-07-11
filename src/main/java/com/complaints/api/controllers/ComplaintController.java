a941f6813bfb866ecd39bda83e62e12139d2d80c
João Neto <joaodeveloper29@gmail.com>
2025-07-11 16:24:16 -0300

    feature: adding list all compliants route

--- src/main/java/com/complaints/api/controllers/ComplaintController.java
+++ src/main/java/com/complaints/api/controllers/ComplaintController.java
@@ -1,24 +1,50 @@
 package com.complaints.api.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.complaints.api.dto.ComplaintRequest;
+import com.complaints.api.dto.ComplaintSummaryResponse;
 import com.complaints.api.model.Complaint;
 import com.complaints.api.repository.ComplaintRepository;
 
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("complaints")
+@RequestMapping("/complaints")
 public class ComplaintController {
   @Autowired
   private ComplaintRepository complaintRepository;
+
+  @GetMapping
+  public ResponseEntity<?> listUserComplaints(@RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "10") int size,
+      Authentication authentication) {
+    String cpf = authentication.getName();
+
+    Pageable pageable = PageRequest.of(page, size);
+    Page<Complaint> complaints = complaintRepository.findAllByCpf(cpf, pageable);
+
+    Page<ComplaintSummaryResponse> responsePage = complaints.map(c -> new ComplaintSummaryResponse(
+        c.getId(),
+        c.getTitle(),
+        c.getDescription(),
+        c.getCreatedAt()));
+
+    return ResponseEntity.ok(responsePage);
+  }
 
   @PostMapping
   public ResponseEntity<?> createComplaint(@Valid @RequestBody ComplaintRequest request,
--- src/main/java/com/complaints/api/repository/ComplaintRepository.java
+++ src/main/java/com/complaints/api/repository/ComplaintRepository.java
@@ -1,11 +1,11 @@
 package com.complaints.api.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.complaints.api.model.Complaint;
 
 public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
-  List<Complaint> findAllByCpf(String cpf);
+  Page<Complaint> findAllByCpf(String cpf, Pageable pageable);
 }
